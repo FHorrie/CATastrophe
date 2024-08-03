cfbdd5a1fd7e625f380016c47b97e8ddc633714c
Airgame1 <finian.horrie@gmail.com>
2024-08-03 13:54:28 +0200

    Added variable bounce physics

--- Assets/Dev/Scripts/PhysTest/BounceVariation.cs
+++ Assets/Dev/Scripts/PhysTest/BounceVariation.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class BounceVariation : MonoBehaviour
 {
     private Rigidbody m_RigidBody;
+    private float m_BounceForce = 1.2f;
 
     private void Awake()
     {
         m_RigidBody = GetComponent<Rigidbody>();
     }
 
+    private void Start()
+    {
+        m_RigidBody.AddForce(0, -15, 0, ForceMode.Impulse);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        m_RigidBody.AddForce(Random.insideUnitCircle.x * 10, 0, Random.insideUnitCircle.y * 10, ForceMode.Impulse);
+        m_RigidBody.AddForce(Random.insideUnitCircle.x * m_BounceForce, 0, Random.insideUnitCircle.y * m_BounceForce, ForceMode.Impulse);
     }
 }
