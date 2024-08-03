aff594ce00c93bc7c838d5234518a98b119e811b
Wannes Degelin Willekens <degelin.wannes@gmail.com>
2024-08-03 20:45:27 +0200

    small changes

--- Assets/Scripts/TrainBehavior.cs
+++ Assets/Scripts/TrainBehavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TrainBehavior : MonoBehaviour
 {
+    [SerializeField] private float trainSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
