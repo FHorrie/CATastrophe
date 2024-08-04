0662ff20c0ea460e6d3e94a27d4610ad16160437
FinianHorrie <finian.horrie@gmail.com>
2024-08-04 13:22:26 +0200

    WIP: Added sounds and asset texture

---
+++ Assets/Dev/Scripts/BlockSoundScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BlockSoundScript : MonoBehaviour
+{
+    private AudioSource m_AudioSource;
+    [SerializeField] private AudioClip[] m_SoundClips;
+
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        int clipIdx = Random.Range(0, m_SoundClips.Length);
+        m_AudioSource.clip = m_SoundClips[clipIdx];
+        m_AudioSource.Play();
+    }
+}
