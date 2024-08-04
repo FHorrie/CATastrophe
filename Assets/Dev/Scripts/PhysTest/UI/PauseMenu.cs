c2aa8f7b441bf8fac5452a065a3002371ece1517
KuYami-TT <huens.nigel@gmail.com>
2024-08-04 15:01:05 +0200

    fix: catapult doesnt move in pause screen

--- Assets/Dev/Scripts/PhysTest/UI/PauseMenu.cs
+++ Assets/Dev/Scripts/PhysTest/UI/PauseMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _pauseMenu;
+    [SerializeField] private GameObject _catapult;
+    [SerializeField] private GameObject _train;
 
     private bool _paused;
 
@@ -34,6 +36,12 @@ public class PauseMenu : MonoBehaviour
         _paused = true;
         Time.timeScale = .0f;
         Cursor.visible = true;
+
+        var catapult = _catapult.GetComponent<CatapultBehavior>();
+        catapult.canMove = false;
+
+        var train = _train.GetComponent<TrainBehavior>();
+        train.canMove = false;
     }
 
     public void ResumeGame()
@@ -42,5 +50,11 @@ public class PauseMenu : MonoBehaviour
         _paused = false;
         Time.timeScale = 1.0f;
         Cursor.visible = false;
+
+        var fuckYou = _catapult.GetComponent<CatapultBehavior>();
+        fuckYou.canMove = true;
+
+        var train = _train.GetComponent<TrainBehavior>();
+        train.canMove = true;
     }
 }
